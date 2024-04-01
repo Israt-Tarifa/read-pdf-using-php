818ebf4e71ef71e1139dd1bf1bb6e1ffe23587eb
Israt-Tarifa <77012964+Israt-Tarifa@users.noreply.github.com>
2024-04-01 11:28:09 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Download Pdf using HTML Link</title>
+</head>
+<body>
+    <center>
+        <h2 style="color: burlywood;">BE A GOOD HUMAN?</h2>
+        <p><b>Click Below to Read the book</b></p>
+        <a href="book.pdf" target="_download">Download PDF Now</a>
+    </center>
+    
+</body>
+</html>
